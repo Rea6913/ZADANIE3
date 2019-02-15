77c5aa6d6b3f4c374d1a3b7e02c557c7e669fcfc
Rea6913 <47655248+Rea6913@users.noreply.github.com>
2019-02-15 22:56:08 +0300

    Update IntegerExtensions.cs

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -7,8 +7,10 @@ namespace PadawansTask3
         public static int Gcd(int a, int b)
         {
             // put your code here
-
-            //throw new NotImplementedException();
+            if (a == 0)
+                return b;
+            else if (b == 0)
+                return a;
             int znak = a / Math.Abs(a);
             int znak1 = b / Math.Abs(b);
             a = Math.Abs(a);
